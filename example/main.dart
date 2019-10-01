af6be448ee43ba9598ed374205cf8f1cc0d7032b
Delton Ding <dsh0416@gmail.com>
2019-10-01 14:12:29 +0900

    Update example

--- example/main.dart
+++ example/main.dart
@@ -1,9 +1,9 @@
 import 'package:euc/euc.dart';
 
 main() {
-  print(EucJP().decoder.convert([
+  print(EucJP().decode([
     164, 170, 164, 207, 164, 232, 164, 166, 192, 164, 179, 166
   ]));
 
-  print(EucJP().encoder.convert("おはよう世界"));
+  print(EucJP().encode("おはよう世界"));
 }
