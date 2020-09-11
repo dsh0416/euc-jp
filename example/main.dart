5eb727c13fc6354c94e5300f3f35b0f2d9e6442f
Delton Ding <dsh0416@gmail.com>
2020-09-11 23:42:42 +0900

    Fix dartfmt

--- example/main.dart
+++ example/main.dart
@@ -3,16 +3,14 @@ import 'package:euc/jis.dart';
 
 main() {
   // EUC-JP Encoding and Decoding
-  print(EucJP().decode([
-    164, 170, 164, 207, 164, 232, 164, 166, 192, 164, 179, 166
-  ]));
+  print(EucJP()
+      .decode([164, 170, 164, 207, 164, 232, 164, 166, 192, 164, 179, 166]));
 
   print(EucJP().encode("おはよう世界"));
 
   // Shift_JIS Encoding and Decoding
-  print(ShiftJIS().decode([
-    130, 168, 130, 205, 130, 230, 130, 164, 144, 162, 138, 69
-  ]));
+  print(ShiftJIS()
+      .decode([130, 168, 130, 205, 130, 230, 130, 164, 144, 162, 138, 69]));
 
   print(ShiftJIS().encode("おはよう世界"));
 }
