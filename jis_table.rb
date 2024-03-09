3746e3646bd6564305161c02dd7a1605cfc8a057
Koga Kazuo <kkazuo@kkazuo.com>
2024-03-10 04:53:25 +0900

    Add Windows-31J supports
    The standard Shift_JIS is rarely used in reality and refers exclusively to Windows-31J.
    In order to better support Windows-31J, we have added the missing characters.
    
    Fix issue #5

--- jis_table.rb
+++ jis_table.rb
@@ -3,14 +3,14 @@ utf = {}
 
 # ASCII Compatible
 (0x00..0x7F).each do |i|
-  jis["0x#{i.to_s(16).rjust(2, '0')}"] = [i].pack('c*').encode("UTF-8", "Shift_JIS")
-  utf[[i].pack('c*').encode("UTF-8", "Shift_JIS").bytes] = [i]
+  jis["0x#{i.to_s(16).rjust(2, '0')}"] = [i].pack('c*').encode("UTF-8", "SJIS")
+  utf[[i].pack('c*').encode("UTF-8", "SJIS").bytes] = [i]
 end
 
 # Half-width Hiragana
 (0xA1..0xDF).each do |i|
-  jis["0x#{i.to_s(16).rjust(2, '0')}"] = [i].pack('c*').encode("UTF-8", "Shift_JIS")
-  utf[[i].pack('c*').encode("UTF-8", "Shift_JIS").bytes] = [i]
+  jis["0x#{i.to_s(16).rjust(2, '0')}"] = [i].pack('c*').encode("UTF-8", "SJIS")
+  utf[[i].pack('c*').encode("UTF-8", "SJIS").bytes] = [i]
 end
 
 # JIS X 0208
@@ -18,7 +18,7 @@ end
   (0x40..0x7E).each do |j|
     begin
       jis["0x#{i.to_s(16).rjust(2, '0')}#{j.to_s(16).rjust(2, '0')}"] = [i, j].pack('c*').encode("UTF-8", "SJIS")
-      utf[[i, j].pack('c*').encode("UTF-8", "Shift_JIS").bytes] = [i, j]
+      utf[[i, j].pack('c*').encode("UTF-8", "SJIS").bytes] = [i, j]
     rescue Encoding::UndefinedConversionError
       # Ignore
     end
@@ -27,19 +27,18 @@ end
   (0x80..0xFC).each do |j|
     begin
       jis["0x#{i.to_s(16).rjust(2, '0')}#{j.to_s(16).rjust(2, '0')}"] = [i, j].pack('c*').encode("UTF-8", "SJIS")
-      utf[[i, j].pack('c*').encode("UTF-8", "Shift_JIS").bytes] = [i, j]
+      utf[[i, j].pack('c*').encode("UTF-8", "SJIS").bytes] = [i, j]
     rescue Encoding::UndefinedConversionError
       # Ignore
     end
   end
 end
 
-
 (0xE0..0xEF).each do |i|
   (0x40..0x7E).each do |j|
     begin
-      jis["0x#{i.to_s(16).rjust(2, '0')}#{j.to_s(16).rjust(2, '0')}"] = [i, j].pack('c*').encode("UTF-8", "Shift_JIS")
-      utf[[i, j].pack('c*').encode("UTF-8", "Shift_JIS").bytes] = [i, j]
+      jis["0x#{i.to_s(16).rjust(2, '0')}#{j.to_s(16).rjust(2, '0')}"] = [i, j].pack('c*').encode("UTF-8", "SJIS")
+      utf[[i, j].pack('c*').encode("UTF-8", "SJIS").bytes] = [i, j]
     rescue Encoding::UndefinedConversionError
       # Ignore
     end
@@ -47,8 +46,26 @@ end
 
   (0x80..0xFC).each do |j|
     begin
-      jis["0x#{i.to_s(16).rjust(2, '0')}#{j.to_s(16).rjust(2, '0')}"] = [i, j].pack('c*').encode("UTF-8", "Shift_JIS")
-      utf[[i, j].pack('c*').encode("UTF-8", "Shift_JIS").bytes] = [i, j]
+      jis["0x#{i.to_s(16).rjust(2, '0')}#{j.to_s(16).rjust(2, '0')}"] = [i, j].pack('c*').encode("UTF-8", "SJIS")
+      utf[[i, j].pack('c*').encode("UTF-8", "SJIS").bytes] = [i, j]
+    rescue Encoding::UndefinedConversionError
+      # Ignore
+    end
+  end
+end
+
+(0xFA..0xFC).each do |i|
+  (0x40..0x7E).each do |j|
+    begin
+      jis["0x#{i.to_s(16).rjust(2, '0')}#{j.to_s(16).rjust(2, '0')}"] = [i, j].pack('c*').encode("UTF-8", "SJIS")
+    rescue Encoding::UndefinedConversionError
+      # Ignore
+    end
+  end
+
+  (0x80..0xFC).each do |j|
+    begin
+      jis["0x#{i.to_s(16).rjust(2, '0')}#{j.to_s(16).rjust(2, '0')}"] = [i, j].pack('c*').encode("UTF-8", "SJIS")
     rescue Encoding::UndefinedConversionError
       # Ignore
     end
